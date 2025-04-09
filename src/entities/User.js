b1b7ef31e189e43d146de13cbd00422425cbf017
Lisset <lissetchopper@gmail.com>
2025-04-08 23:35:05 -0600

    Feat(backend)::.env / ConexionBD / ModelUser

---
+++ src/entities/User.js
@@ -0,0 +1,30 @@
+const { EntitySchema } = require('typeorm');
+
+module.exports = new EntitySchema({
+  name: 'User',
+  tableName: 'users',
+  columns: {
+    id: {
+      primary: true,
+      type: 'int',
+      generated: true,
+    },
+    name: {
+      type: 'varchar',
+    },
+    email: {
+      type: 'varchar',
+      unique: true,
+    },
+    password: {
+      type: 'varchar',
+    },
+  },
+  relations: {
+    contacts: {
+      type: 'one-to-many',
+      target: 'Contact',
+      inverseSide: 'user',
+    },
+  },
+});
